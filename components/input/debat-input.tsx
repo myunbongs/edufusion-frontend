4fdf65df68c57b6a1b5342e4fad67346a453a4d8
myunbongs <myunbongs@naver.com>
2023-07-09 01:53:14 +0900

    hotfix: 🚑  배포 이슈 수정 (#5)

--- components/input/debat-input.tsx
+++ components/input/debat-input.tsx
@@ -1,7 +1,7 @@
 const DebatInput = () => {
     return (<>
         <div className="mb-4 mt-8">
-            <textarea htmlFor="contents" id="contents" name="contents" rows="4" required placeholder="나는 이렇게 생각해." className="w-full rounded border border-gray-700 focus:border-indigo-500 outline-none py-1 px-3 leading-6 transition-colors duration-200 dark:text-white"></textarea>
+            <textarea htmlFor="contents" id="contents" name="contents" rows={4} required placeholder="나는 이렇게 생각해." className="w-full rounded border border-gray-700 focus:border-indigo-500 outline-none py-1 px-3 leading-6 transition-colors duration-200 dark:text-white"></textarea>
         </div> 
     </>);
 }
--- components/input/qna-input.tsx
+++ components/input/qna-input.tsx
@@ -1,7 +1,7 @@
 const QnAInput = () => {
     return (<>
         <div className="mb-4 mt-8">
-            <textarea htmlFor="contents" id="contents" name="contents" rows="4" required placeholder="디지털 피싱이 뭐야?" className="w-full rounded border border-gray-700 focus:border-indigo-500 outline-none py-1 px-3 leading-6 transition-colors duration-200 dark:text-white"></textarea>
+            <textarea htmlFor="contents" id="contents" name="contents" rows={4} required placeholder="디지털 피싱이 뭐야?" className="w-full rounded border border-gray-700 focus:border-indigo-500 outline-none py-1 px-3 leading-6 transition-colors duration-200 dark:text-white"></textarea>
         </div> 
     </>);
 }
